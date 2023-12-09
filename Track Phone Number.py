299421a8b46d1be59cba2e238a6e1ac39bed7e21
Sudharshan Kumaresan <138243791+Sudharshank-cpu@users.noreply.github.com>
2023-12-09 11:49:35 +0530

    Track Phone Number.py

--- Track Phone Number.py
+++ Track Phone Number.py
@@ -1,12 +1,11 @@
 import phonenumbers
 from phonenumbers import geocoder
-#                        "             "-Enter your Phone Number from Here
-phno1=phonenumbers.parse("+917092732950")
-phno2=phonenumbers.parse("+919344123335")
-phno3=phonenumbers.parse("+12136574429")
-phno4=phonenumbers.parse("+911234567890")
+phNo=input("Enter your Phone Number: ")
+phno1=phonenumbers.parse(phNo)
+#To check it
+#phno2=phonenumbers.parse("+12136574429")
+#It gets only at International Phone Numbers.Can't track all Numbers
 print("\nPhone Numbers Location\n")
 print(geocoder.description_for_number(phno1,"en"))
-print(geocoder.description_for_number(phno2,"en"))
-print(geocoder.description_for_number(phno3,"en"))
-print(geocoder.description_for_number(phno4,"en"))
+#print(geocoder.description_for_number(phno2,"en"))
+#To install "pip install phonenumbers"
